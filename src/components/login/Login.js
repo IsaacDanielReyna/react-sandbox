a9e9d5132debdb55c728cc08d06e0c6e7bf1309b
Isaac Daniel Reyna <isaacdanielreyna@gmail.com>
2021-08-01 16:19:21 -0500

    Added type password to Password's TextField.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -93,6 +93,7 @@ export default function Login() {
                                 id='password'
                                 label='Password'
                                 variant='filled'
+                                type="password"
                                 required
                                 fullWidth
                                 error={hasInvalidPassword}
