ef55e59368e83175175f001a80637b145028ae63
Isaac Daniel Reyna <isaacdanielreyna@gmail.com>
2023-02-26 21:30:48 -0600

    Updated MiniProfile.js and user-service.js

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,11 +1,7 @@
 import api from '../utils/api';
 import Keycloak from 'keycloak-js';
 
-const keycloak = new Keycloak({
-    url: 'https://keycloak.isaacreyna.com',
-    realm: 'demo',
-    clientId: 'local-auth',
-});
+const keycloak = new Keycloak('/keycloak.json');
 
 const initKeycloak = (onAuthenticatedCallback) => {
     keycloak
